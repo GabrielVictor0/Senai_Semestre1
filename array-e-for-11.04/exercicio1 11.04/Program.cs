8a47042be530e91785370cdcf8a4cc9463c56275
GabrielVictor0 <bielgvsa@gmail.com>
2023-10-17 07:56:05 -0300

    .

---
+++ array-e-for-11.04/exercicio1 11.04/Program.cs
@@ -0,0 +1,34 @@
+﻿//criar um array de strings para armazenar 3 carros
+// string[] carros = new string[3];
+
+// for (int i = 0; i < 3; i++)
+// {
+//     Console.WriteLine($"Informe o nome do carro: ");    
+//     carros[i] = Console.ReadLine();    
+// }
+
+// Console.WriteLine($"Lista dos carros cadastrados: ");
+
+// for (var i = 0; i < 3; i++)
+// {
+//     Console.WriteLine($"Nome do carro: {carros[i]}");    
+// }
+
+// foreach (var item in carros)
+// {
+//     Console.WriteLine(item);    
+// }
+
+int[] numeros = new int[5];
+
+for(int i = 0; i <=4; i++)
+{
+    Console.WriteLine($"Informe um número inteiro qualquer: ");
+    numeros [i] = int.Parse(Console.ReadLine());
+}
+
+foreach(int item in numeros)
+{
+    Console.WriteLine($" O dobro dos números informados: {item*2}");
+    
+}
---
+++ array-e-for-11.04/exercicio2 11.04/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] nome = new string[5];
+
+int[] idade = new int[5];
+
+for(var i = 0; i < 5; i++)
+{
+    Console.WriteLine($" {i+1}º Informe seu nome: ");
+    nome[i] = Console.ReadLine();
+    
+    Console.WriteLine(@$" {i+1}º Informe sua idade: "
+    
+    );
+    idade[i] = int.Parse(Console.ReadLine());
+    
+}
+
+for( var i = 0; i < 5; i++)
+{
+    Console.WriteLine(@$" 
+   
+{i+1}º nome e idade: ");
+    
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($" nome: {nome[0]}");
+    Console.ResetColor();
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($" idade: {idade[0]}");
+    Console.ResetColor();
+    
+}
+
